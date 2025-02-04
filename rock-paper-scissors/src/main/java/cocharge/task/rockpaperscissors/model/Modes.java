4f5127660572872122c1c0c1cb1ea0c6e42ef902
zeyad amer <zeyadamer285@gmail.com>
2025-02-04 02:49:21 +0200

    refactoring enums to decide from int

--- rock-paper-scissors/src/main/java/cocharge/task/rockpaperscissors/model/Modes.java
+++ rock-paper-scissors/src/main/java/cocharge/task/rockpaperscissors/model/Modes.java
@@ -3,6 +3,19 @@ package cocharge.task.rockpaperscissors.model;
 public enum Modes {
     Decider,
     MinimalRequirement,
-    SingePlayer
+    SingePlayer;
 
-}
+
+    public static Modes fromInt(int userInput) {
+        switch (userInput) {
+            case 1:
+                return Modes.MinimalRequirement;
+            case 2:
+                return Modes.SingePlayer;
+            case 3:
+                return Modes.Decider;
+            default:
+                return null;
+        }
+    }
+    }
--- rock-paper-scissors/src/main/java/cocharge/task/rockpaperscissors/model/RockPaperScissors.java
+++ rock-paper-scissors/src/main/java/cocharge/task/rockpaperscissors/model/RockPaperScissors.java
@@ -3,5 +3,14 @@ package cocharge.task.rockpaperscissors.model;
 public enum RockPaperScissors {
     ROCK,
     PAPER,
-    SCISSORS
+    SCISSORS;
+
+    public static RockPaperScissors fromInt(int userChoice){
+        switch (userChoice){
+            case 1: return ROCK;
+            case 2: return PAPER;
+            case 3: return SCISSORS;
+            default: return null;
+        }
+    }
 }
